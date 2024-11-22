0fc80d9473ffe9f6f3e324e0d55a5d5514abe6ff
Thang0226 <thang.nd0226@gmail.com>
2024-11-22 11:46:56 +0700

    add GetCustomersCountByCity procedure

--- mysql-stored-procedure.sql
+++ mysql-stored-procedure.sql
@@ -37,3 +37,39 @@ delimiter ;
 
 
 CALL getCusByID(175);
+
+
+
+DELIMITER //
+
+CREATE PROCEDURE GetCustomersCountByCity(
+
+    IN  in_city VARCHAR(50),
+
+    OUT total INT
+
+)
+
+BEGIN
+
+    SELECT COUNT(customerNumber)
+    INTO total		-- assign COUNT(...) to variable 'total'
+    FROM customers
+    WHERE city = in_city;
+    
+END//
+
+DELIMITER ;
+
+
+CALL GetCustomersCountByCity('lyon', @total);
+SELECT @total;
+
+
+
+
+
+
+
+
+
